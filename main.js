324898846ab74c1e36c5ddbb6fe50a19a86fd1f2
ferdyrn <69664765+ferdyrn@users.noreply.github.com>
2021-02-11 17:42:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['6281515860089'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.owner = ['6283173108368'] // Put your number here
+global.mods = ['6283173108368'] // Want some help?
+global.prems = ['6283173108368'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -86,11 +86,11 @@ conn.handler = async function (m) {
       let user
       if (user = global.DATABASE._data.users[m.sender]) {
         if (!isNumber(user.exp)) user.exp = 0
-        if (!isNumber(user.limit)) user.limit = 10
+        if (!isNumber(user.limit)) user.limit = 30
         if (!isNumber(user.lastclaim)) user.lastclaim = 0
       } else global.DATABASE._data.users[m.sender] = {
         exp: 0,
-        limit: 10,
+        limit: 30,
         lastclaim: 0,
       }
       
@@ -193,7 +193,7 @@ conn.handler = async function (m) {
         }
 
         m.isCommand = true
-        let xp = 'exp' in plugin ? parseInt(plugin.exp) : 9 // XP Earning per command
+        let xp = 'exp' in plugin ? parseInt(plugin.exp) : 15 // XP Earning per command
         if (xp > 99) m.reply('Ngecit -_-') // Hehehe
         else m.exp += xp
         if (!isPrems && global.DATABASE._data.users[m.sender].limit < m.limit * 1 && plugin.limit) {
@@ -269,8 +269,8 @@ conn.handler = async function (m) {
     }
   }
 }
-conn.welcome = 'Hai, @user!\nSelamat datang di grup @subject'
-conn.bye = 'Selamat tinggal @user!'
+conn.welcome = 'Hai, @user!\nSelamat datang di grup @subject *Semoga betah ya* 😁'
+conn.bye = 'Selamat tinggal @user! *Semoga tenang disana* 👋'
 conn.onAdd = async function ({ m, participants }) {
   let chat = global.DATABASE._data.chats[m.key.remoteJid]
   if (!chat.welcome) return
@@ -348,8 +348,8 @@ conn.on('close', () => {
 
 global.dfail = (type, m, conn) => {
   let msg = {
-    rowner: 'Perintag ini hanya dapat digunakan oleh _*OWWNER!1!1!*_',
-    owner: 'Perintah ini hanya dapat digunakan oleh _*Owner Bot*_!',
+    rowner: 'Perintah ini hanya dapat digunakan oleh _*OWNER*_ !',
+    owner: 'Perintah ini hanya dapat digunakan oleh _*Owner Bot*_ !',
     mods: 'Perintah ini hanya dapat digunakan oleh _*Moderator*_ !',
     premium: 'Perintah ini hanya untuk member _*Premium*_ !',
     group: 'Perintah ini hanya dapat digunakan di grup!',
