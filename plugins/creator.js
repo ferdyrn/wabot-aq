1e31a95b154778be365b36797949d264a9f99dc4
ferdyrn <69664765+ferdyrn@users.noreply.github.com>
2021-02-11 18:45:27 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6283173108368', 'Ferdy', m)
-  this.sendContact(m.chat, '0', '6283173108368', m)
+  this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
